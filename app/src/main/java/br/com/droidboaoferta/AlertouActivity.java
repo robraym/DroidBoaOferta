d82eb6ee62e25274956f063bae5475b157f8bf34
Robraym <robson.raymundo@gmail.com>
2026-07-21 10:01:18 -0300

    fix: refresh accent colors across screens

--- app/src/main/java/br/com/droidboaoferta/AlertouActivity.java
+++ app/src/main/java/br/com/droidboaoferta/AlertouActivity.java
@@ -6,9 +6,21 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 abstract class AlertouActivity extends AppCompatActivity {
+    private String appliedAccentMode;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        appliedAccentMode = AccentColorController.getSavedMode(this);
         AccentColorController.apply(this);
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        String savedAccentMode = AccentColorController.getSavedMode(this);
+        if (!savedAccentMode.equals(appliedAccentMode)) {
+            recreate();
+        }
     }
 }
